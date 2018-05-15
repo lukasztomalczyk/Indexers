7b8ccdeaecbe3ea98fac20ea9c3a8560f8e2018e
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-05-15 20:32:36 +0200

    Indexers example

---
+++ Books.cs
@@ -0,0 +1,45 @@
+﻿namespace Indexers
+{
+    class Books
+    {
+        private readonly string[] _listOfBooks = new string[10];
+
+        public Books()
+        {
+            for (int i = 0; i < _listOfBooks.Length; i++)
+            {
+                _listOfBooks[i] = $"this is {i} item from array";
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < _listOfBooks.Length)
+                {
+                    return _listOfBooks[index];
+                }
+
+                return null;
+            }
+            set => _listOfBooks[index] = value;
+        }
+
+        public int this[string name]
+        {
+            get
+            {
+                int index = 0;
+                
+                while (index < _listOfBooks.Length)
+                {
+                    if (_listOfBooks[index] == name) return index;
+                    index++;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Dynamic;
+using System.Net.Mime;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks.Dataflow;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var book = new Books();
+
+            Console.WriteLine(book[3]);
+
+            book[3] = "we changed item";
+
+            Console.WriteLine(book[3]);
+
+            var getItemByValue = book["we changed item"];
+
+            Console.WriteLine(getItemByValue);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
